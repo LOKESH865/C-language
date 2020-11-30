5aa8ff592549981d40b290b7c3df901877536a00
LOKESH865 <72989897+LOKESH865@users.noreply.github.com>
2020-11-30 23:25:04 +0530

    Add files via upload

---
+++ three number sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a,b,c,sum=0;
+printf("Enter the value of three numbers/n");
+scanf("%d %d %d",&a,&b,&c);
+sum=a+b+c;
+printf("sum=%d",sum);
+getch();
+
+}
